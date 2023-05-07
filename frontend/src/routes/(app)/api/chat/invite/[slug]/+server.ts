74581d5f59238f548afe7cc6d72dd753cb72b28e
jhizdahr <jhizdahr@student.21-school.ru>
2023-05-07 22:11:38 +0300

    nothing good

--- frontend/src/routes/(app)/api/chat/invite/[slug]/+server.ts
+++ frontend/src/routes/(app)/api/chat/invite/[slug]/+server.ts
@@ -1,20 +1,33 @@
 import chatService from '$lib/services/chatService';
+import { addAuthHeader, baseUrl, createBackendUrl, removeApiEndpoint } from '$lib/services/settings';
 import { redirect } from '@sveltejs/kit';
 
 export async function GET({ cookies, params }) {
     const id = params.slug;
     const userId = cookies.get('user-id') as string;
-    const chat = await chatService.getChatById(id);
-    console.log(chat, id);
-    if (!chat || chat.banList.find((user) => user.id === id)) {
-        throw redirect(303, '/404');
-    }
-    if (chat.privacyMode === 'protected') {
-        throw redirect(308, '/chatrooms/invite/protected/' + id);
-    } else {
-        await chatService.addMembers([userId], id);
-        throw redirect(308, '/chatrooms');
-    }
+    const authToken = cookies.get('user-token');
 
+    const endpoint = 'chat/';
+    const baseUrlWithEndpoint = new URL(endpoint, baseUrl);
+    const baseIdUrl = new URL('chatbyid/' + id, baseUrlWithEndpoint);
+    const backendEndpoint = removeApiEndpoint(baseIdUrl.pathname);
+
+    
+    const response = await fetch(createBackendUrl(backendEndpoint), {
+        headers: {
+            ...addAuthHeader(authToken || '')
+        }
+    });
+    const parsed = await response.text();
+        const chat = JSON.parse(parsed);
+        if (!chat || chat.banList.find((user) => user.id === id)) {
+            throw redirect(303, '/404');
+        }
+        if (chat.privacyMode === 'protected') {
+            throw redirect(308, '/chatrooms/invite/protected/' + id);
+        } else {
+            await chatService.addMembers([userId], id); //here potential problem with cookies
+            throw redirect(308, '/chatrooms');
+        }
     return new Response(null);
 }
