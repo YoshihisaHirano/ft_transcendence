aaae1c862ec83ef8fa318f78736f21b8b655fe3e
Anelle Alannys <anastasia7febr@gmail.com>
2023-05-12 17:05:59 +0400

    [FE-16] fixes chat invite link issue

--- frontend/src/routes/(app)/api/users/logout/+server.ts
+++ frontend/src/routes/(app)/api/users/logout/+server.ts
@@ -3,19 +3,19 @@ export async function DELETE({ cookies }) {
     cookies.delete('user-id', {
         path: '/',
         maxAge: 0,
-        httpOnly: true,
+        // httpOnly: true,
         secure: false
     });
     cookies.delete('user-token', {
         path: '/',
         maxAge: 0,
-        httpOnly: true,
+        // httpOnly: true,
         secure: false
     });
     cookies.delete('user-login', {
         path: '/',
         maxAge: 0,
-        httpOnly: true,
+        // httpOnly: true,
         secure: false
     });
     return new Response(null);
