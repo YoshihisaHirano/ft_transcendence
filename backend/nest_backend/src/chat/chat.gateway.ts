681027fc4cfa26ac66c13e3fff03c98ce6b0388a
n0ring <menshikh_92@mail.ru>
2023-05-13 12:47:56 +0400

    docket setup

--- backend/nest_backend/src/chat/chat.gateway.ts
+++ backend/nest_backend/src/chat/chat.gateway.ts
@@ -16,7 +16,7 @@ import { MuteService } from './services/mute.service';
 	namespace: '/chat',
 	cors: {
 		credentials: true,
-		origin: "http://192.168.10.3:5176",
+		origin: "http:172.20.0.11//:5176",
 		methods: ['GET', 'POST'],
 	  	transports: ['websocket'],
 	}
