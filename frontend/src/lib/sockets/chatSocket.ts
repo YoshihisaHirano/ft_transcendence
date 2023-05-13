aab2779cdb99faa0fec3b70fe2156db5d7423c8d
Anelle Alannys <anastasia7febr@gmail.com>
2023-05-13 19:08:42 +0400

    [FE-19] host config via env file

--- frontend/src/lib/sockets/chatSocket.ts
+++ frontend/src/lib/sockets/chatSocket.ts
@@ -1,4 +1,4 @@
 import ioClient from 'socket.io-client';
 
-const chatSocket = ioClient('http://10.18.120.249:3000/chat', { transports: ['websocket']});
+const chatSocket = ioClient(`http://${import.meta.env.VITE_HOST_IP}:3000/chat`, { transports: ['websocket']});
 export const chatIo = chatSocket;
--- frontend/src/lib/sockets/gameSocket.ts
+++ frontend/src/lib/sockets/gameSocket.ts
@@ -1,4 +1,4 @@
 import ioClient from 'socket.io-client';
 
-const gameSocket = ioClient('http://10.18.120.249:3000/game', { transports: ['websocket']});
+const gameSocket = ioClient(`http://${import.meta.env.VITE_HOST_IP}:3000/game`, { transports: ['websocket']});
 export const gameIo = gameSocket;
