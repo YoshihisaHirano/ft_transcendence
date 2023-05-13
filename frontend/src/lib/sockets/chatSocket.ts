af64dbdd84e645de621faf38ec6da7d889ae25b0
Anelle Alannys <anastasia7febr@gmail.com>
2023-05-13 12:45:36 +0400

    [FE-18] changes ip addresses

--- frontend/src/lib/sockets/chatSocket.ts
+++ frontend/src/lib/sockets/chatSocket.ts
@@ -1,4 +1,4 @@
 import ioClient from 'socket.io-client';
 
-const chatSocket = ioClient('http://192.168.10.3:3000/chat');
+const chatSocket = ioClient('http://172.20.0.10:3000/chat');
 export const chatIo = chatSocket;
